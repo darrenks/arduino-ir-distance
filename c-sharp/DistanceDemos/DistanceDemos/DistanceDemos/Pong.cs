746431dae56eff777007c9a518dea33e48a9f6db
Harish Vaidyanathan <harishsrinivas89@gmail.com>
2012-10-03 15:15:50 -0400

    Setting up the interfacefor Pong

--- c-sharp/DistanceDemos/DistanceDemos/DistanceDemos/Pong.cs
+++ c-sharp/DistanceDemos/DistanceDemos/DistanceDemos/Pong.cs
@@ -6,15 +6,23 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Threading.Tasks;
+using System.Threading;
 
 namespace DistanceDemos
 {
     public partial class Pong : Form
     {
         DistanceSensors sensors;
+        String textToDisplay;
+        //PictureBox paddle1;
+        //PictureBox paddle2;
         public Pong()
         {
             InitializeComponent();
+            textToDisplay = "";
+            //paddle1 = pictureBox1;
+            //paddle2 = pictureBox2;
         }
 
         private void Pong_Load(object sender, EventArgs e)
@@ -28,21 +36,40 @@ namespace DistanceDemos
         void sensors_DistancesChanged(double[] dists)
         {
             // normalize distance
-            float x = (float)dists[0];
-            float y = (float)dists[1];
-            float z = (float)dists[2];
-
-            DisplayPanel.Refresh();
+            float analog1 = (float)dists[0];
+            float analog2 = (float)dists[1];
+            //String textToDisplay = "";
+            textToDisplay = Convert.ToString(analog1) + ", " + Convert.ToString(analog2);
+            //textBox1.Text = textToDisplay;
+            //textBox1.Enabled = false;
+            //DisplayPanel.Refresh();
         }
 
         private void Update()
         {
-
+            Invoke(new MethodInvoker(delegate
+            {
+                textBox1.Text = textToDisplay;
+                textBox1.Enabled = false;
+                DisplayPanel.Refresh();
+            }));
+            
         }
 
         private void DisplayPanel_Paint(object sender, PaintEventArgs e)
         {
-            e.Graphics.FillRectangle(Brushes.Blue, 10, 10, 50, 50);
+            int i = 10;
+            
+            /*for (; i < 210; i++)
+            {
+                e.Graphics.FillRectangle(Brushes.Blue, 10, i, 50, 50);
+                //Task.Factory.StartNew(()=> {});
+                //Thread.Sleep(500);
+                //e.Graphics.Clear
+                
+            }*/
+           //e.Graphics.FillEllipse(Brushes.Green, 100, 100, 50, 50);
         }
+
     }
 }
