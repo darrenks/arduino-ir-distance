d02010e266367d004a55b19b478bdb9244df1b6e
Harish Vaidyanathan <harishsrinivas89@gmail.com>
2012-10-05 15:04:08 -0400

    the pong motion incorporation

--- c-sharp/DistanceDemos/DistanceDemos/DistanceDemos/Pong.cs
+++ c-sharp/DistanceDemos/DistanceDemos/DistanceDemos/Pong.cs
@@ -15,14 +15,24 @@ namespace DistanceDemos
     {
         DistanceSensors sensors;
         String textToDisplay;
-        //PictureBox paddle1;
-        //PictureBox paddle2;
+        float analog1;
+        float analog2;
+        int user1Score;
+        int user2Score;
+        bool direction;
+        int pongX;
+        int pongY;
+        int reAnalog1, reAnalog2;
         public Pong()
         {
             InitializeComponent();
             textToDisplay = "";
-            //paddle1 = pictureBox1;
-            //paddle2 = pictureBox2;
+            user1Score = 0;
+            user2Score = 0;
+            direction = true;
+            pongX = 270;
+            pongY = 179;
+
         }
 
         private void Pong_Load(object sender, EventArgs e)
@@ -30,22 +40,19 @@ namespace DistanceDemos
             sensors = new DistanceSensors();
             sensors.DistancesChanged += new DistanceSensors.DistancesChangedHandler(sensors_DistancesChanged);
             sensors.Connect();
-
-            Update();
         }
         void sensors_DistancesChanged(double[] dists)
         {
             // normalize distance
-            float analog1 = (float)dists[0];
-            float analog2 = (float)dists[1];
-            //String textToDisplay = "";
-            textToDisplay = Convert.ToString(analog1) + ", " + Convert.ToString(analog2);
-            //textBox1.Text = textToDisplay;
-            //textBox1.Enabled = false;
-            //DisplayPanel.Refresh();
+            if (dists.Length > 0) analog1 = (float)dists[0];
+            if (dists.Length > 1) analog2 = (float)dists[1];
+            textToDisplay = analog1.ToString("0.0") + ", " + analog2.ToString("0.0");
+            UpdateFrame();
+            movePaddes();
+            moveBall();
         }
 
-        private void Update()
+        private void UpdateFrame()
         {
             Invoke(new MethodInvoker(delegate
             {
@@ -55,20 +62,58 @@ namespace DistanceDemos
             }));
             
         }
+        private void movePaddes()
+        {
+            Invoke(new MethodInvoker(delegate
+            {
+                int i = 0;
+                //12,169 
+                //starts from 8 ends at 30. translate it to movements between 22 steps to measure
+                //8 to 19 -> 0 to 165; 20-30 -> 166 to 330
+                reAnalog1 = (int)analog1;
+                reAnalog2 = (int)analog2;
+                reAnalog1 = (reAnalog1 - 8) * (165 / 11);
+                reAnalog2 = (reAnalog2 - 8) * (165 / 11);
+                if (reAnalog1 > 330) reAnalog1 = 330;
+                if (reAnalog2 > 330) reAnalog2 = 330;
+
+                //if(reAnalog1>=0 && reAnalog1<=165) 
+                this.pictureBox1.Location = new System.Drawing.Point(12, reAnalog1);
+                this.pictureBox2.Location = new System.Drawing.Point(503, reAnalog2);
+                DisplayPanel.Refresh();
+            }));
+            
+        }
+        private void moveBall()
+        {
+            //270 179
+            
+            this.pictureBox3.Location = new System.Drawing.Point(pongX,pongY);
+            pongX--;
+            pongY++;
+            Invoke(new MethodInvoker(delegate
+            {
+                if (direction = true)
+                {
+                    this.pictureBox3.Location = new System.Drawing.Point(pongX, pongY);
+                    pongX--;
+                    pongY++;
+                    //if(pongX==78 && pictureBox1.Location
+                    DisplayPanel.Refresh();
+                }
+                if (pongX == 78 && reAnalog1 <= pongY - 12)
+                {
+                    pongX++;
+                    pongY--;
+                    this.pictureBox3.Location = new System.Drawing.Point(pongX, pongY);
+                    //DisplayPanel.Refresh();
+                }
+            }));
+        }
 
         private void DisplayPanel_Paint(object sender, PaintEventArgs e)
         {
             int i = 10;
-            
-            /*for (; i < 210; i++)
-            {
-                e.Graphics.FillRectangle(Brushes.Blue, 10, i, 50, 50);
-                //Task.Factory.StartNew(()=> {});
-                //Thread.Sleep(500);
-                //e.Graphics.Clear
-                
-            }*/
-           //e.Graphics.FillEllipse(Brushes.Green, 100, 100, 50, 50);
         }
 
     }
